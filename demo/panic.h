2487f972cf065b96e740523cc2e1d55c2aabd677
fuse <fuse@fzort.org>
2019-04-01 19:16:17 -0300

    a better panic

--- demo/panic.h
+++ demo/panic.h
@@ -1,3 +1,17 @@
 #pragma once
 
-void panic(const char *fmt, ...);
+#include <cstdio>
+#include <cstdlib>
+
+inline void panic(const char *fmt)
+{
+    fprintf(stderr, "%s", fmt);
+    abort();
+}
+
+template<typename ... Args>
+inline void panic(const char *fmt, const Args& ... args)
+{
+    fprintf(stderr, fmt, args...);
+    abort();
+}
